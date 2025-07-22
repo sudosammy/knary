34120037addbdc55551c3054d7fd039442bf47c0
sudosammy <sudosammy@users.noreply.github.com>
2025-07-22 22:24:36 +0800

    fix panic

--- libknary/dns.go
+++ libknary/dns.go
@@ -335,7 +335,11 @@ func GuessIP(domain string) (string, error) {
 	// query the tld's nameserver for our knary domain and extract the glue record from additional information
 	kMsg := new(dns.Msg)
 	kMsg.SetQuestion(dns.Fqdn(domain), dns.TypeNS)
-	answ, _, _ := new(dns.Client).Exchange(kMsg, tldDNS+":53")
+	answ, _, err := new(dns.Client).Exchange(kMsg, tldDNS+":53")
+
+	if err != nil || answ == nil {
+		return "", errors.New("DNS exchange failed for domain: " + domain + " with nameserver: " + tldDNS + ". Have you configured a glue record for your domain? Has it propagated? You can set EXT_IP to bypass this but... do you know what you're doing?")
+	}
 
 	if len(answ.Extra) == 0 {
 		return "", errors.New("No 'Additional' section in NS lookup for: " + domain + " with nameserver: " + tldDNS + " Have you configured a glue record for your domain? Has it propagated? You can set EXT_IP to bypass this but... do you know what you're doing?")
