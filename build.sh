bc2d67ad5acfc0e9192b881606aee69213a9c4ee
sudosammy <sudosammy@users.noreply.github.com>
2026-08-08 18:37:14 +0800

    gh action for deps bump

--- build.sh
+++ build.sh
@@ -1,29 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
-version=$1
+version=${1:-}
 if [[ -z "$version" ]]; then
 	echo "usage: $0 <version>"
 	echo "i.e. $0 4.0.1"
 	exit 1
 fi
 
-platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+platforms=("windows/amd64" "windows/arm64" "linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64")
 
 for platform in "${platforms[@]}"
 do
-	platform_split=(${platform//\// })
-	GOOS=${platform_split[0]}
-	GOARCH=${platform_split[1]}
-	output_name='knary-'$version'-'$GOOS'-'$GOARCH
-	if [ $GOOS = "windows" ]; then
-		output_name+='.exe'
-	fi  
-
-	echo "Building $GOOS version..."
-	env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o $output_name main.go
-	if [ $? -ne 0 ]; then
-		echo 'An error has occurred! Aborting the script execution...'
-		exit 1
+	IFS=/ read -r goos goarch <<< "$platform"
+	output_name="knary-${version}-${goos}-${goarch}"
+	if [[ "$goos" == "windows" ]]; then
+		output_name+=".exe"
 	fi
+
+	echo "Building $goos/$goarch version..."
+	env GOOS="$goos" GOARCH="$goarch" CGO_ENABLED=0 go build -tags timetzdata -o "$output_name" .
 done
